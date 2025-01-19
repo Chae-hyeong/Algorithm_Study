7d202f4b56c182f8192a9a63b44406da32d53b59
chhy0504 <145464690+Chae-hyeong@users.noreply.github.com>
2025-01-19 17:48:26 +0900

    refactor: 주석 추가

--- 백준/Silver/23253. 자료구조는 정말 최고야/자료구조는 정말 최고야.py
+++ 백준/Silver/23253. 자료구조는 정말 최고야/자료구조는 정말 최고야.py
@@ -2,14 +2,15 @@ import sys
 
 input = sys.stdin.readline
 
+# 입력
 N, M = map(int, input().split())
-valid = True
+valid = True # 정렬 여부
 
 for _ in range(M):
-    int(input())
+    int(input()) # 더미 크기 필요 없음
     stack = list(map(int, input().split()))
 
-    if stack != sorted(stack, reverse=True):
+    if stack != sorted(stack, reverse=True): # 내림차순 정렬이 아닌 경우
         valid = False
 
 print("Yes" if valid else "No")
